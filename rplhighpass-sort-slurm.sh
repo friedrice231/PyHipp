567d873e86d97256e63c0fe3bc7a1bbab33f2535
AWS ParallelCluster user <ec2-user@ip-10-0-14-52.ap-southeast-1.compute.internal>
2023-10-28 14:07:22 +0000

    - added commands to interface with diff environments

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -13,6 +13,11 @@
 #SBATCH -e rplhighpass-sort-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=`/data/src/PyHipp/envlist.py`
+conda activate $envarg
+
 python -u -c "import PyHipp as pyh; \
 import time; \
 pyh.RPLHighPass(saveLevel=1); \
@@ -21,5 +26,8 @@ mountain_batch.mountain_batch(); \
 from PyHipp import export_mountain_cells; \
 export_mountain_cells.export_mountain_cells(); \
 print(time.localtime());"
+
+conda deactivate 
+/data/src/PyHipp/envlist.py $envarg
 
 #aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:119845913857:awsnotify --message "RPLS1JobDone"
